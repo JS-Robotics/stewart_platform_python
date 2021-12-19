d46747e403233c958bb3f47998153bf442fb7065
Sondre <sjohnsen.valle@gmail.com>
2021-12-19 22:01:33 +0100

    Fixed the computational bug

--- platform_plot.py
+++ platform_plot.py
@@ -11,24 +11,26 @@ import matplotlib.animation as animation
 
 def distance_calc(ry, ss, h: float = 0,  shift: float = 0, debug: bool = False):
     counter = 1  # Function is 1 indexed in order to work with documentation
+    ss = ss / 2  # Getting half the length of ss, explained in documentation
     gamma = 120 * pi / 180
     applied_gamma = gamma
-    print(f"gamma == {gamma}")
     edges = 6
     x_point = [0]*edges
     y_point = [0]*edges
     z_point = [h]*edges
     for edge in range(edges):  # The platform has 6 corners
         if counter % 2 == 0:
-            # if debug:
             applied_gamma = gamma * (counter/2 - 1)
-            print(f"counter par = {counter} - applied gamma factor: {counter / 2 - 1}, applied gamma = {applied_gamma}")
+            if debug:
+                print(f"counter par = {counter} - applied gamma factor: {counter / 2 - 1}, "
+                      f"applied gamma = {applied_gamma}, sign: {(-1)**edge}")
         elif counter % 2 != 0:
-            # if debug:
-            applied_gamma = gamma * ((counter-1)/2)
-            print(f"counter odd = {counter} - applied gamma factor: {(counter - 1) / 2}, applied gamma = {applied_gamma}")
-        x_point[edge] = ry*cos(applied_gamma + shift) + ss * cos(applied_gamma - (-1)**counter * pi/2 + shift)
-        y_point[edge] = ry*sin(applied_gamma + shift) + ss * sin(applied_gamma - (-1)**counter * pi/2 + shift)
+            applied_gamma = gamma * ((counter - 1)/2)
+            if debug:
+                print(f"counter odd = {counter} - applied gamma factor: {(counter - 1) / 2}, "
+                      f"applied gamma = {applied_gamma}, sign: {(-1)**edge}")
+        x_point[edge] = ry*cos(applied_gamma + shift) + ss * cos(applied_gamma - (-1)**edge * pi/2 + shift)
+        y_point[edge] = ry*sin(applied_gamma + shift) + ss * sin(applied_gamma - (-1)**edge * pi/2 + shift)
         counter = counter + 1
     x_point.append(x_point[0])
     y_point.append(y_point[0])
@@ -53,20 +55,20 @@ r_f = 90
 s_f = 30
 
 x_p, y_p, z_p = distance_calc(r_g, s_s, h=12)
-# x_f, y_f, z_f = distance_calc(r_f, s_f, shift=60*pi/180)
+x_f, y_f, z_f = distance_calc(r_f, s_f, shift=60*pi/180)
 
-print(x_p)
-print(y_p)
-print(z_p)
-print("-----------")
+# print(x_p)
+# print(y_p)
+# print(z_p)
+# print("-----------")
 # print(x_f)
 # print(y_f)
 # print(z_f)
 
 
 line1, = ax.plot(x_p, y_p, z_p, color='red', marker='o', label='Quaternion rotation 90deg')
-# line2, = ax.plot(x_f, y_f, z_f, color='red', marker='o', label='Quaternion rotation 90deg')
+line2, = ax.plot(x_f, y_f, z_f, color='red', marker='o', label='Quaternion rotation 90deg')
 line3, = ax.plot(0, 0, 12, color='blue', marker='o', label='Quaternion rotation 90deg')
-# ax.legend(handles=[line1, line2, line3])
-ax.legend(handles=[line1, line3])
+ax.legend(handles=[line1, line2, line3])
+
 plt.show()
