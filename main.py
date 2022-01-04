1c460bcb1ac75384fb27b77702d85718c69dce6e
Sondre <sjohnsen.valle@gmail.com>
2022-01-04 19:03:08 +0100

    Set plot data to true in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from stewart_platform import StewartPlatform
 
 platform = StewartPlatform(fixed_long=90, fixed_short=40, dynamic_long=70, dynamic_short=30)
-platform.animate(plot_wave_date=False)
+platform.animate(plot_wave_date=True)
